43d12f6bdf43d69e807674370b315518fa54d90f
zhaozhengyu@advanpro.hk <zhaozhengyu@advanpro.hk>
2020-02-13 17:41:49 +0800

    文档

--- src/main/java/com/zzy/vertx/core/message/AbstractMessageConvert.java
+++ src/main/java/com/zzy/vertx/core/message/AbstractMessageConvert.java
@@ -4,12 +4,13 @@ import org.springframework.http.MediaType;
 
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public abstract class AbstractMessageConvert implements VertxHttpMessageConvert {
   public static final Charset DEFAULT_CHARSET = Charset.forName(StandardCharsets.UTF_8.name());
-  private volatile List<MediaType> supportedMediaTypes = Collections.emptyList();
+  private volatile List<MediaType> supportedMediaTypes = new ArrayList<>();
 
 
   @Override
